fa2a061f2f75a45509813471ac63c6aa93eb1ec4
Pavel <enthery@mail.ru>
2024-11-18 17:50:03 +0300

    Database

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ import Header from "./components/header/Header";
 import PizzaBlock from "./components/pizza-block/PizzaBlock";
 
 import db from "./assets/db/pizza.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://673b4458339a4ce4451b6ca1.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories />
+            {/* <Categories /> */}
             <Sort />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {db.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
