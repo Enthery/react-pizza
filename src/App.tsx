f42abc8ea927d93e77c00395497de9aa5b32ee40
Pavel <enthery@mail.ru>
2024-12-19 13:11:18 +0300

    Оптимизируем сборку JS-файла. Code Splitting

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@ import "./scss/app.scss";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
-import FullPizza from "./pages/FullPizza";
 
 import MainLayout from "./layouts/MainLayout";
+import React, { Suspense } from "react";
+
+const Cart = React.lazy(() => import('./pages/Cart'))
+const FullPizza = React.lazy(() => import('./pages/FullPizza'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route 
+        path="/cart" 
+        element={
+        <Suspense fallback={<div>Загрузка...</div>}>
+          <Cart />
+        </Suspense>} />
+        <Route path="/pizza/:id" element={<Suspense fallback={<div>Загрузка...</div>}>
+          <FullPizza />
+        </Suspense>} />
+        <Route path="*" element={<Suspense fallback={<div>Загрузка...</div>}>
+          <NotFound />
+        </Suspense>} />
       </Route>
     </Routes>
   );
